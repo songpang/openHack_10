6d7448f8e81ed39a2582d8b504f85b7fe366b448
songpang <scc6920@naver.com>
2019-06-28 05:24:02 +0900

    klaytn complete

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,216 @@ import "tui-calendar/dist/tui-calendar.css";
 import "tui-date-picker/dist/tui-date-picker.css";
 import "tui-time-picker/dist/tui-time-picker.css";
 
+var today = new Date();
+var countMonth = 0;
+const Caver = require('caver-js');
+const caver = new Caver(new Caver.providers.HttpProvider('https://api.baobab.klaytn.net:8651/'));
+const ERC20ABI = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "approve",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "sender",
+				"type": "address"
+			},
+			{
+				"name": "recipient",
+				"type": "address"
+			},
+			{
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFrom",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "recipient",
+				"type": "address"
+			},
+			{
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"name": "spender",
+				"type": "address"
+			}
+		],
+		"name": "allowance",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Approval",
+		"type": "event"
+	}
+]
+
+const myContract = new caver.klay.Contract(ERC20ABI, '0x1a05d6F86dFA69A556780D5800a1fd405EbE44c2', {
+  from: '0x72c5f48a39bb3915b8b8699b7182ba933a287288', // default from address
+  gasPrice: '25000000000' // default gas price in peb, 25 Gpeb in this case
+});
+// caver.klay.getBalance('0x72c5f48a39bb3915b8b8699b7182ba933a287288').then(console.log);
+console.log(caver.klay.accounts.privateKeyToAccount('0xe699acbd53c7df36787d39b6138eb44a8e916ea3c3f2ab01565e191ab1370c2e'));
+
+const feePayerPrivateKey = "0xe699acbd53c7df36787d39b6138eb44a8e916ea3c3f2ab01565e191ab1370c2e"
+caver.klay.accounts.wallet.add(feePayerPrivateKey, '0x72c5f48a39bb3915b8b8699b7182ba933a287288')
+caver.klay.defaultAccount = '0x72c5f48a39bb3915b8b8699b7182ba933a287288';
+myContract.options.from = '0x72c5f48a39bb3915b8b8699b7182ba933a287288'; // default from address
+myContract.options.gasPrice = '25000000000'; // default gas price in peb
+myContract.options.gas = 5000000; // provide as fallback always 5M gas
+
+// console.log(myContract.options);
+// console.log(myContract.methods.totalSupply().call());
+
+// myContract.methods.transfer('0x036161669e02b74cd9b217fcc277062196dfba52', 10000000000000).call()
+//   .then(function(receipt) {
+//     console.log(receipt);
+//   });
+
+  // // transfer => unknown account
+  myContract.methods.transfer('0x036161669e02b74cd9b217fcc277062196dfba52', caver.utils.toPeb('1', 'KLAY')).
+  send()
+  .then(function(receipt) {
+    console.log(receipt);
+  });
+  
+
 const calendar = new Calendar("#calendar", {
   defaultView: "month",
   useCreationPopup: true,
@@ -34,8 +244,8 @@ const calendar = new Calendar("#calendar", {
   },
   calendars: [
     {
-      id: "1",
-      name: "My Calendar",
+      id: '1',
+      name: "Company",
       color: "#ffffff",
       bgColor: "#9e5fff",
       dragBgColor: "#9e5fff",
@@ -43,12 +253,29 @@ const calendar = new Calendar("#calendar", {
     },
     {
       id: "2",
-      name: "Company",
+      name: "University",
       color: "#00a9ff",
       bgColor: "#00a9ff",
       dragBgColor: "#00a9ff",
       borderColor: "#00a9ff"
-    }
+    },
+    {
+      id: "3",
+      name: "Friends",
+      color: "#00a9ff",
+      bgColor: "#00a9ff",
+      dragBgColor: "#00a9ff",
+      borderColor: "#00a9ff"
+    },
+    {
+      id: "4",
+      name: "",
+      color: "#00a9ff",
+      bgColor: "#00a9ff",
+      dragBgColor: "#00a9ff",
+      borderColor: "#00a9ff"
+    },
+
   ],
   template: {
     popupDetailLocation: function(schedule) {
@@ -62,7 +289,7 @@ const App = {
     let groupname = $('#groupname').val();
     console.log(groupname);
     const plusUl = document.createElement("li");
-    plusUl.innerHTML = '<a style="display: flex; justify-content: space-between;"><p>'+groupname+'</p><img src = "../assets/img/자산 7.png"/></a>';
+    plusUl.innerHTML = '<a style="display: flex; justify-content: space-between;"><p style="font-size: 18px;">'+groupname+'</p><img src = "../assets/img/자산 7.png"/></a>';
     document.getElementById("a").appendChild(plusUl);
   },
   changeMonth: function() {
@@ -80,26 +307,65 @@ const App = {
   clear: function() {
     calendar.clear();
     calendar.createSchedules(schedules, true);
-    // calendar.render();
+    calendar.render();
   },
   changePrev: function() {
+    countMonth -= 1;
     calendar.prev();
+    this.nowgetdate(countMonth);
   },
   changeNext: function() {
+    countMonth += 1;
     calendar.next();
+    this.nowgetdate(countMonth);
   },
-  changeHide: function() {
-    calendar.render();
+  changeToday: function() {
+    calendar.today();
+  },
+  destroy: function() {
+    calendar.destroy();
+  },
+  rerender: function() {
+    calendar._initialize();
+  },
+  changeCalendar: function() {
+    calendar.toggleSchedules('Major Lecture', true, true);
+  },
+  nowgetdate: function(add = 0) {
+    // let timenow = (calendar.getDate()._date + ' ').split(' ').slice(1, 4).join(' ');
+    let mm = String(today.getMonth()+1+add) + '월'; //January is 0!
+    let yyyy = String(today.getFullYear()) + '년 ';
+    let nowdate = yyyy + mm;
+    
+    const plusUl = document.createElement("div");
+    plusUl.innerHTML = '<p style = "font-size: 50px; text-color: gray">'+nowdate+'</p>';
+    const setdateElement = document.getElementById("setdate");
+
+    while (setdateElement.firstChild) {
+        setdateElement.removeChild(setdateElement.firstChild);
+    }
+
+    setdateElement.appendChild(plusUl);
+    console.log(nowdate);
+
   }
+  // inputtoval: function() {
+  //   let input = document.getElementById("groupname").value; 
+  //   let temp = input;
+  //   // console.log(temp);
+  // }
 };
 
 window.App = App;
+
+App.nowgetdate();
 
 calendar.createSchedules([
   {
     id: "1",
     calendarId: 'Major Lecture',
     title: "자료 구조",
+    location: "한빛관 304호",
     category: "time",
     start: "2019-06-23T10:30:00",
     end: "2019-06-23T12:30:00"
@@ -110,7 +376,6 @@ calendar.createSchedules([
     title: "건강과 영양",
     location: '구리시',
     category: "time",
-    dueDateClass: "",
     start: "2019-06-24T14:30:00",
     end: "2019-06-24T16:30:00",
     isReadOnly: true // schedule is read-only
@@ -164,3 +429,8 @@ calendar.on("beforeDeleteSchedule", scheduleData => {
   schedule.end = end;
   calendar.deleteSchedule(schedule.id, schedule.calendarId);
 });
+
+// $('#plusModal').on('hidden.bs.modal', function (e) {
+//   console.log('modal close');
+//   $('#groupname')[0].reset()
+// });
