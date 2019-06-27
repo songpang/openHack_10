4d1a40d684998dd23ca8992dac07c41e0ce080dc
배희수 <huisu0715@khu.ac.kr>
2019-06-27 18:58:24 +0900

    restructuring server

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index'
+      template: './src/index.html'
     }),
     new CleanWebpackPlugin(['dist']),
     new MiniCssExtractPlugin()
--- server/app.js
+++ server/app.js
@@ -3,22 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var clientApp = path.join(__dirname, '../client/site/_static');
 
-var indexRouter = require('../client/src/index');
-var usersRouter = require('../client/src/users');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, '../client/src'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+// app.engine('html', require('ejs').renderFile);
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('*', express.static(clientApp))
+app.use(express.static(path.join(__dirname, 'public/assets')));
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
