c1ef6ff760acfdc8c2d11399bc5d2d7a525d1e50
배희수 <huisu0715@khu.ac.kr>
2019-06-27 12:48:10 +0900

    path changed, delete some files

--- server/app.js
+++ server/app.js
@@ -5,8 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var clientApp = path.join(__dirname, '../client/site/_static');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('../client/src/index');
+var usersRouter = require('../client/src/users');
 
 var app = express();
 
